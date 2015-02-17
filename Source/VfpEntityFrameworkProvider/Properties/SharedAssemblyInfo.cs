6512b14cb22b4f8e7ae57362fbba27c123fea326
Tom Brothers <tombrothers@outlook.com>
2015-02-17 17:54:13 -0500

    Added Registry settings for Visual Studio 2015. Added build script.

--- Source/VfpEntityFrameworkProvider/Properties/SharedAssemblyInfo.cs
+++ Source/VfpEntityFrameworkProvider/Properties/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyConfiguration("")]
